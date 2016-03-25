df7976daf4654135104db8d84870a05f3d5968d6
Cole DeMan <cl422670@dal.ca>
2016-03-25 17:42:52 -0300

    added copy of my resume

--- resources/views/resume.blade.php
+++ resources/views/resume.blade.php
@@ -6,6 +6,7 @@
     <div class="section black">
         <div class="container grey-text">
             <h3>@yield('title')</h3>
+            <embed src="{{URL::asset('pdf/resume_rough.pdf')}}" width="800px" height="2100px">
         </div>
     </div>
 @endsection
