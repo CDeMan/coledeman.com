d1c2d9184f113a9caeb7aa2c31c396a55ed0f72a
Cole DeMan <cl422670@dal.ca>
2016-03-25 02:24:45 -0300

    fixed colour on drop down menus

--- resources/views/partials/contentdisplay.blade.php
+++ resources/views/partials/contentdisplay.blade.php
@@ -1,6 +1,7 @@
 @extends('partials.skeleton')
 
 @section('nav_color', 'purple darken-3')
+@section('drop_txt_color', 'grey-text text-darken-4')
 
 @section('body')
 
--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -6,10 +6,10 @@
                 <ul id="nav-mobile" class="right">
                     <li>
                         <a href="#" class="dropdown-button" data-beloworigin="true" data-activates="dropdown4">About Cole</a>
-                        <ul id="dropdown4" class="dropdown-content">
-                            <li><a href="/resume">Resume</a></li>
-                            <li><a href="http://github.coledeman.com">Github</a></li>
-                            <li><a href="#">Link3</a></li>
+                        <ul id="dropdown4" class="dropdown-content purple lighten-2">
+                            <li><a class="@yield('drop_txt_color')" href="/resume">Resume</a></li>
+                            <li><a class="@yield('drop_txt_color')" href="http://github.coledeman.com">Github</a></li>
+                            <li><a class="@yield('drop_txt_color')" href="#">Link3</a></li>
                         </ul>
                         <li><a href="http://web.cs.dal.ca/~deman">Dal Website</a></li>
                     </li>
