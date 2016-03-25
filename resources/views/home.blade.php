d94103a4c2eb11188d99654decc29964f9ba929d
Cole DeMan <cl422670@dal.ca>
2016-03-25 00:29:27 -0300

    created own views and added materialize

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('partials.skeleton')
+
+@section('title', 'Home')
+
+@section('body')
+    <h1>Hi this is a test</h1>
+@endsection
---
+++ resources/views/partials/skeleton.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>@yield('title') - Cole DeMan's Site</title>
+
+    <link href="{{ URL::asset('css/materialize.css') }}" rel="stylesheet" type="text/css" media="screen,projection"/>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    @yield('styles')
+    @yield('scripts')
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
+<body>
+@yield('body')
+<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/js/materialize.min.js"></script>
+@yield('endscripts')
+</body>
+</html>
