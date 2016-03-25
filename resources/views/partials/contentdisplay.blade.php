8fffdb28d521de7b590587fc5b357b3173064869
Cole DeMan <cl422670@dal.ca>
2016-03-25 16:07:16 -0300

    added google analytics

--- resources/views/partials/contentdisplay.blade.php
+++ resources/views/partials/contentdisplay.blade.php
@@ -10,4 +10,27 @@
 
 @endsection
 
-
+@section('footer')
+    <div class="section grey darken-3">
+    <div class="container">
+        <div class="row">
+            <div class="col l6 s12">
+                <h5 class="white-text">Contact Me:</h5>
+                <a href="http://www.google.com/recaptcha/mailhide/d?k=01_jPVwtIu9dNWiukOMSOCIA==&amp;c=AA9xec6x11FiE5Ci7Tkc4wtZpnveyFuClJvO22b15Mo=" onclick="window.open('http://www.google.com/recaptcha/mailhide/d?k\07501_jPVwtIu9dNWiukOMSOCIA\75\75\46c\75AA9xec6x11FiE5Ci7Tkc4wtZpnveyFuClJvO22b15Mo\075', '', 'toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=0,width=500,height=300'); return false;" title="Reveal this e-mail address">Reveal Email Address</a>
+            </div>
+            <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Links</h5>
+                <ul>
+                    <li><a class="grey-text text-lighten-3" href="http://twitter.coledeman.com">Twitter</a></li>
+                    <li><a class="grey-text text-lighten-3" href="http://Github.coledeman.com">Github</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="footer-copyright">
+        <div class="container">
+            © 2016 Cole DeMan
+        </div>
+    </div>
+    </div>
+@endsection
--- resources/views/partials/skeleton.blade.php
+++ resources/views/partials/skeleton.blade.php
@@ -7,6 +7,17 @@
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     @yield('styles')
     @yield('scripts')
+    <script src='https://www.google.com/recaptcha/api.js'></script>
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-75629904-1', 'auto');
+        ga('send', 'pageview');
+
+    </script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 </head>
 <body>
@@ -14,7 +25,11 @@
     <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
     <script src="{{URL::asset('js/materialize.js')}}"></script>
 
+
     {{--<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/js/materialize.min.js"></script>--}}
 @yield('endscripts')
 </body>
+<footer class="footer-copyright">
+    @yield('footer')
+</footer>
 </html>
