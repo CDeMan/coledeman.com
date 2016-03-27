7ed6ba71db3edd9ae63d395340f957a75086b01e
Cole DeMan <cl422670@dal.ca>
2016-03-27 00:36:12 -0300

    made cats faster

--- resources/views/cat.blade.php
+++ resources/views/cat.blade.php
@@ -12,10 +12,8 @@
     <div class="grey darken-4">
 
         <?php
-        for ($x = 0; $x <= 10; $x++) {
-            $content=file_get_contents('http://thecatapi.com/api/images/get?format=html&api_key=NzY3NjA');
+            $content=file_get_contents('http://thecatapi.com/api/images/get?format=html&api_key=NzY3NjA&results_per_page=21');
             echo $content;
-        }
         ?>
     </div>
 
