af1bf60a20bb686b5c8d21e8bc049f236e447453
Cole DeMan <cl422670@dal.ca>
2016-03-30 01:14:01 -0300

    changed delorian ipsum to lorem ipsum

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -6,71 +6,34 @@
     <div class="section grey darken-4 grey-text">
         <div class="container grey-text ">
             <h3>@yield('title')</h3>
-            <p>
-                Hey, Doc, we better back up, we don't have enough roads to get up to 88. Don't worry. As long as
-                you hit that wire with the connecting hook at precisely 88 miles per hour, the instance the lightning
-                strikes the tower, everything will be fine. No, Marty, we've already agreed that having information
-                about
-                the future could be extremely dangerous. Even if your intentions are good, they could backfire
-                drastically.
-                Whatever you've got to tell me I'll find out through the natural course of time. Marty you gotta come
-                back
-                with me. What a nightmare. Oh, I've been so worried about you ever since you ran off the other night.
-                Are
-                you okay? I'm sorry I have to go. Isn't he a dream boat? This Saturday night, mostly clear, with some
-                scattered clouds. Lows in the upper forties. Who the hell is John F. Kennedy? McFly. Well yeah, you know
-                we
-                have two of them. It's cold, damn cold. Ha, ha, ha, Einstein, you little devil. Einstein's clock is
-                exactly
-                one minute behind mine, it's still ticking. How could I have been so careless. One point twenty-one
-                gigawatts. Tom, how am I gonna generate that kind of power, it can't be done, it can't. Well, Marty, I
-                want
-                to thank you for all your good advise, I'll never forget it. George, help me, please. What, what is it
-                hot?
-                What, I don't get what happened. Whoa, whoa, kid, kid, stop, stop, stop, stop. You okay, is everything
-                alright? Great good, good, Lorraine, I had a feeling about you two. Don't worry. As long as you hit that
-                wire with the connecting hook at precisely 88 miles per hour, the instance the lightning strikes the
-                tower,
-                everything will be fine. She's just trying to keep you respectable. Well uh, good, fine. George McFly?
-                Oh,
-                he's kinda cute and all, but, well, I think a man should be strong, so he could stand up for himself,
-                and
-                protect the woman he loves. Don't you? He's an absolute dream. You wanna a Pepsi, pall, you're gonna pay
-                for
-                it. A block passed Maple, that's John F. Kennedy Drive. Pa, what is it? What is it, Pa? That's right.
-                Wait a
-                minute. Wait a minute, Doc. Are you telling me that it's 8:25? Ah well, sort of. I have to tell you
-                about
-                the future. Who are you calling spook, pecker-wood. yes, Joey just loves being in his playpen. he cries
-                whenever we take him out so we just leave him in there all the time. Well Marty, I hope you like
-                meatloaf.
-                Right. Precisely. I'm gonna get that son-of-a-bitch. Let him go, Biff, you're drunk. Yeah, I'm- mayor.
-                Now
-                that's a good idea. I could run for mayor. Bet your ass it works. Quiet down, I'm sure the car is fine.
-                It's
-                a board with wheels. Doc? Right. Ahh. Well gee, I don't know. Yeah, but I never picked a fight in my
-                entire
-                life. Okay. No, bastards. I have to tell you about the future. God dammit, I'm late. What's the meaning
-                of
-                this. Where the hell are they. George: you ever think of running for class president? Marty, such a nice
-                name. Whoa, whoa, Biff, what's that? Just turn around, McFly, and walk away. Are you deaf, McFly? Close
-                the
-                door and beat it. Never? How about a ride, Mister? Oh, no no no, I never uh, I never let anybody read my
-                stories. You have this thing hooked up to the car? Say that again. About how far ahead are you going?
-                Marty,
-                you made it. Let me show you my plan for sending you home. Please excuse the crudity of this model, I
-                didn't
-                have time to build it to scale or to paint it. Marty, I always wear a suit to the office. You alright?
-                Aw
-                yeah, everything is great. I know, and all I could say is I'm sorry. Good evening, I'm Doctor Emmett
-                Brown.
-                I'm standing on the parking lot of Twin Pines Mall. It's Saturday morning, October 26, 1985, 1:18 a.m.
-                and
-                this is temporal experiment number one. C'mon, Einy, hey hey boy, get in there, that a boy, in you go,
-                get
-                down, that's it. Why am I always the last one to know about these things. What a nightmare. Let him go,
-                Biff, you're drunk.
-            </p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Hanc quoque iucunditatem, si vis, transfer in
+                animum; Quid ait Aristoteles reliquique Platonis alumni? Duo Reges: constructio interrete. Faceres tu
+                quidem, Torquate, haec omnia; Nam et complectitur verbis, quod vult, et dicit plane, quod intellegam; At
+                iam decimum annum in spelunca iacet. Et quidem illud ipsum non nimium probo et tantum patior,
+                philosophum loqui de cupiditatibus finiendis. Si enim ad populum me vocas, eum. Aufert enim sensus
+                actionemque tollit omnem. Ab his oratores, ab his imperatores ac rerum publicarum principes extiterunt.
+                Quare attende, quaeso. </p>
+
+            <p>Bonum liberi: misera orbitas. Vitiosum est enim in dividendo partem in genere numerare. Egone quaeris,
+                inquit, quid sentiam? Quid dubitas igitur mutare principia naturae? </p>
+
+            <p>Ab hoc autem quaedam non melius quam veteres, quaedam omnino relicta. Verum hoc idem saepe faciamus. Hi
+                curatione adhibita levantur in dies, valet alter plus cotidie, alter videt. Ut necesse sit omnium rerum,
+                quae natura vigeant, similem esse finem, non eundem. Omnia contraria, quos etiam insanos esse
+                vultis. </p>
+
+            <p>Atque his de rebus et splendida est eorum et illustris oratio. Quid enim de amicitia statueris utilitatis
+                causa expetenda vides. Si verbum sequimur, primum longius verbum praepositum quam bonum. Qui non
+                moveatur et offensione turpitudinis et comprobatione honestatis? Age sane, inquam. Sic enim censent,
+                oportunitatis esse beate vivere. Sed tamen est aliquid, quod nobis non liceat, liceat illis. At iste non
+                dolendi status non vocatur voluptas. </p>
+
+            <p>Vestri haec verecundius, illi fortasse constantius. Reguli reiciendam; Videsne quam sit magna dissensio?
+                Atqui iste locus est, Piso, tibi etiam atque etiam confirmandus, inquam; Nec vero alia sunt quaerenda
+                contra Carneadeam illam sententiam. At, si voluptas esset bonum, desideraret. Nam Pyrrho, Aristo,
+                Erillus iam diu abiecti. Tibi hoc incredibile, quod beatissimum. </p>
+
+
         </div>
     </div>
 @endsection
