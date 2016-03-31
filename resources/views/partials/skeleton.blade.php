2f906a42266756dfcda28ab3413e1d340ae5faf9
Cole DeMan <cl422670@dal.ca>
2016-03-30 23:21:22 -0300

    change error check for google analytics disabling

--- resources/views/partials/skeleton.blade.php
+++ resources/views/partials/skeleton.blade.php
@@ -10,7 +10,6 @@
     @yield('styles')
     @yield('scripts')
     <script src='https://www.google.com/recaptcha/api.js'></script>
-    <script>
         <?php
         $whitelist = array(
                 '127.0.0.1',
@@ -20,6 +19,8 @@
         if(!in_array($_SERVER['REMOTE_ADDR'], $whitelist)){
             // not valid
         ?>
+
+    <script>
         (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
                     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
                 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -27,13 +28,14 @@
 
         ga('create', 'UA-75629904-1', 'auto');
         ga('send', 'pageview');
+    </script>
         <?php
             }else{
-            echo "<h1>The disabling google worked</h1>";
+            echo "<!--The disabling google worked -->";
         }
                 ?>
 
-    </script>
+
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <meta name="description" content="Cole DeMan's Website">
     <meta name="keywords" content="computer science, resume, coder, programmer, laravel, dalhousie">
