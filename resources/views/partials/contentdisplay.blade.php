a83cb9024f694aa5de2f2c3ce37a0e6ae35d0d59
Cole DeMan <cl422670@dal.ca>
2016-04-02 02:04:11 -0300

    added my name

--- resources/views/partials/contentdisplay.blade.php
+++ resources/views/partials/contentdisplay.blade.php
@@ -17,6 +17,7 @@
             <div class="col l6 s12">
                 <h5 class="white-text">Contact Me:</h5>
                 <a href="http://www.google.com/recaptcha/mailhide/d?k=01_mzbDQ0mGTnZ4OkqGFqOZA==&amp;c=Y5IF4Eiy9BI-8b2ofCaAakBinuOn7UnVXObpRcBFOzc=" onclick="window.open('http://www.google.com/recaptcha/mailhide/d?k\07501_mzbDQ0mGTnZ4OkqGFqOZA\75\75\46c\75Y5IF4Eiy9BI-8b2ofCaAakBinuOn7UnVXObpRcBFOzc\075', '', 'toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=0,width=500,height=300'); return false;" title="Reveal this e-mail address">Cole DeMan's Email</a>
+                <p>This webite was created by Cole DeMan using laravel.</p>
             </div>
             <div class="col l4 offset-l2 s12">
                 <h5 class="white-text">Links</h5>
