7334aab3bb04ccda0f9d607b6130d73cf9aeb201
Cole DeMan <cl422670@dal.ca>
2016-06-27 03:26:35 -0300

    reformated code

--- resources/views/apps/shitgen.blade.php
+++ resources/views/apps/shitgen.blade.php
@@ -3,38 +3,39 @@
 @section('title', 'Shit Generator')
 
 @section('content')
-        <div class="container grey-text">
-            <h2>@yield('title')</h2>
-            <?php
-            $noun = $data[1];
-            $adj = $data[0]
-            ?>
-            <p>
-                👌👀👌👀👌👀👌👀👌👀 {!!$adj!!} {!!$noun!!} {!!$adj!!} {!!$noun!!}👌 thats ✔ some {!!$adj!!}
-                👌👌{!!$noun!!}
-                right👌👌there👌👌👌 right✔there ✔✔if i do ƽaү so my self 💯 i say so 💯 thats what im talking about
-                right there right there (chorus: ʳᶦᵍʰᵗ ᵗʰᵉʳᵉ) mMMMMᎷМ💯 👌👌 👌НO0ОଠOOOOOОଠଠOoooᵒᵒᵒᵒᵒᵒᵒᵒᵒ👌 👌👌 👌 💯
-                👌 👀 👀 👀 👌👌{!!$adj!!} {!!$noun!!}
-            </p>
-            <div class="card-panel  grey darken-1">
-                <div class="form-group black-text">
-                    <form>
-                        <div class="row">
-                            <div class="col s6">
-                                <label class="black-text">Adjective:</label>
-                                <input type="text" name="adj" value="{!!$adj!!}">
-                            </div>
-                            <div class="col s6">
-                                <label class="black-text">Noun:</label>
-                                <input type="text" name="noun" value="{!!$noun!!}">
-                            </div>
-                            <input type="submit" value="Generate">
+    <div class="container grey-text">
+        <h2>@yield('title')</h2>
+        <?php
+        $noun = $data[1];
+        $adj = $data[0]
+        ?>
+        <p>
+            👌👀👌👀👌👀👌👀👌👀 {!!$adj!!} {!!$noun!!} {!!$adj!!} {!!$noun!!}👌 thats ✔ some {!!$adj!!}
+            👌👌{!!$noun!!}
+            right👌👌there👌👌👌 right✔there ✔✔if i do ƽaү so my self 💯 i say so 💯 thats what im talking about
+            right there right there (chorus: ʳᶦᵍʰᵗ ᵗʰᵉʳᵉ) mMMMMᎷМ💯 👌👌 👌НO0ОଠOOOOOОଠଠOoooᵒᵒᵒᵒᵒᵒᵒᵒᵒ👌 👌👌 👌 💯
+            👌 👀 👀 👀 👌👌{!!$adj!!} {!!$noun!!}
+        </p>
+        <div class="card-panel  grey darken-1">
+            <div class="form-group black-text">
+                <form>
+                    <div class="row">
+                        <div class="col s6">
+                            <label class="black-text">Adjective:</label>
+                            <input type="text" name="adj" value="{!!$adj!!}">
                         </div>
-                    </form>
-                    <a href="{{ action('apps\shitgen\AppController@listArchive') }}">See all previously used words</a>
-                </div>
+                        <div class="col s6">
+                            <label class="black-text">Noun:</label>
+                            <input type="text" name="noun" value="{!!$noun!!}">
+                        </div>
+                        <input type="submit" value="Generate">
+                    </div>
+                </form>
+
+                <a href="{{ action('apps\shitgen\AppController@listArchive') }}">See all previously used words</a>
             </div>
         </div>
+    </div>
 
 @endsection
 
